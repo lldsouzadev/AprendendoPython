22608d5d8f0771bc655b958b7a30693448ba09ed
Leandro Luiz de Souza <54249923+lldsouzadev@users.noreply.github.com>
2023-01-21 16:13:16 -0300

    Create Python36Mundo2.py

---
+++ Python36Mundo2.py
@@ -0,0 +1,21 @@
+# Aula téorica
+
+#carro.siga()
+#if carro.esquerda():
+#    carro.siga()
+#    carro.direita()
+#    carro.siga()
+#    carro.direita()
+#    carro.esquerda()
+#    carro.siga()
+#    carro.direita()
+#    carro.siga()
+#elif carro.direita():
+#    carro.siga()
+#    carro.esquerda()
+#    carro.siga()
+#    carro.esquerda()
+#    carro.siga()
+#else:
+#    carro.siga()
+#carro.para()
